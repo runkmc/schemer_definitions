c29d29afb4c33eee966d60245881b67b4a9d5cad
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-01-29 14:06:35 -0800

    Initial Commit, through chapter 2 of the Little Schemer

---
+++ little_schemer_defs.rkt
@@ -0,0 +1,16 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+       (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? a (car lat)) (member? a (cdr lat))) #t))))
