b16b4ac441cf16a8b19a7590b21d423eee4c5ba2
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-02-09 13:20:18 -0800

    Complete Chapter 7, Little Schemer

--- little_schemer_defs.rkt
+++ little_schemer_defs.rkt
@@ -303,3 +303,34 @@
       ((atom? aexp) (number? aexp))
       (else (and (numbered? (car aexp)) (numbered? (car (cdr aexp))))))))
 
+(define 1st-sub-exp
+  (lambda (aexp)
+    (car aexp)))
+
+(define 2nd-sub-exp
+  (lambda (aexp)
+    (car (cdr (cdr aexp)))))
+
+(define operator
+  (lambda (aexp)
+    (car (cdr aexp))))
+
+(define value
+  (lambda (nexp)
+    (cond
+      ((atom? nexp) nexp)
+      ((eq? (operator nexp) (quote +)) (o+ (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp))))
+      ((eq? (operator nexp) (quote *)) (o* (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp))))
+      (else (o+ (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp)))))))
+
+(define sero?
+  (lambda (n)
+    (null? n)))
+
+(define edd1
+  (lambda (n)
+    (cons '() n)))
+
+(define zub1
+  (lambda (n)
+    (cdr n)))
