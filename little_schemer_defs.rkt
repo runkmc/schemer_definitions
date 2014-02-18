740fe1e8d9ebeab4731ac0590c5b38d8ea5225ab
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-02-18 13:54:31 -0800

    More progress in chapter 8

--- little_schemer_defs.rkt
+++ little_schemer_defs.rkt
@@ -507,5 +507,20 @@
       ((eq? oldR (car lat)) (cons oldR (cons new (multiinsertLR new oldL oldR (cdr lat)))))
       (else (cons (car lat) (multiinsertLR new oldL oldR (cdr lat)))))))
 
+(define multiinsertLR&co
+  (lambda (new oldL oldR lat col)
+    (cond
+      ((null? lat) (col '() 0 0))
+      ((eq? (car lat) oldL) (multiinsertLR&co new oldL oldR (cdr lat) (lambda (newlat L R) (col (cons new (cons oldL newlat)) (add1 L) R))))
+      ((eq? (car lat) oldR) (multiinsertLR&co new oldL oldR (cdr lat) (lambda (newlat L R) (col (cons oldR (cons new newlat)) L (add1 R)))))
+      (else (multiinsertLR&co new oldL oldR (cdr lat) (lambda (newlat L R) (col (cons (car lat) newlat) L R)))))))
 
-      
+(define evens-only
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((even? (car l)) (cons (car l) (evens-only (cdr l))))
+         (else (evens-only (cdr l)))))
+      (else (cons (evens-only (car l)) (evens-only (cdr l)))))))
