9a4fcfee49968354bf8acac3e998b576ed0db26e
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-03-09 22:36:11 -0700

    Continue chapter 10

--- little_schemer_defs.rkt
+++ little_schemer_defs.rkt
@@ -591,3 +591,59 @@
       ((null? table) (table-f name))
       (else (lookup-in-entry name (car table) (lambda (name) lookup-in-table name (cdr table) table-f))))))
 
+(define expression-to-action
+  (lambda (e)
+    (cond
+      ((atom? e) (atom-to-action e))
+      (else (list-to-action e)))))
+
+(define atom-to-action
+  (lambda (e)
+    (cond
+      ((number? e) *const)
+      ((eq? e #t) *const)
+      ((eq? e #f) *const)
+      ((eq? e (quote cons)) *const)
+      ((eq? e (quote car)) *const)
+      ((eq? e (quote cdr)) *const)
+      ((eq? e (quote null?)) *const)
+      ((eq? e (quote eq?)) *const)
+      ((eq? e (quote atom?)) *const)
+      ((eq? e (quote zero?)) *const)
+      ((eq? e (quote add1)) *const)
+      ((eq? e (quote sub1)) *const)
+      ((eq? e (quote number?)) *const)
+      (else *identifier))))
+
+(define list-to-action
+  (lambda (e)
+    (cond
+      ((atom? (car e))
+       (cond
+         ((eq? (car e) (quote quote)) *quote)
+         ((eq? (car e) (quote lambda)) *lambda)
+         ((eq? (car e) (quote cond)) *cond)
+         (else *application)))
+      (else *application))))
+
+(define value
+  (lambda (e)
+    (meaning e (quote ()))))
+
+(define meaning
+  (lambda (e table)
+    ((expression-to-action e) e table)))
+
+(define *const
+  (lambda (e table)
+    (cond
+      ((number? e) e)
+      ((eq? e #t) #t)
+      ((eq? e #f) #f)
+      (else (build (quote primitive) e)))))
+
+(define *quote
+  (lambda (e table)
+    (text-of e)))
+
+(define text-of second)
