38f0230217454e01adeabc984e40244892f30232
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-03-12 00:19:46 -0700

    Prepare for chapter 11

---
+++ seasoned_schemer_defs.rkt
@@ -0,0 +1,15 @@
+;; Preparing ourselves
+
+(define atom?
+  (lambda (x)
+	(and (not (pair? x)) (not (null? x)))))
+
+(define add1
+  (lambda (n)
+	(+ n 1)))
+
+(define sub1
+  (lambda (n)
+	(- n 1)))
+
+;; Begin Chapter 11!
