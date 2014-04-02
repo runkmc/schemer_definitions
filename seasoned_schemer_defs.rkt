77542c65b4b3884b90e00e12d0013a5f10fa5045
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-04-01 23:53:27 -0700

    Continue chapter 11

--- seasoned_schemer_defs.rkt
+++ seasoned_schemer_defs.rkt
@@ -13,3 +13,38 @@
 	(- n 1)))
 
 ;; Begin Chapter 11!
+
+;; We remember member
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? a (car lat)) (member? a (cdr lat)))))))
+
+(define old-two-in-a-row?
+  (lambda (lat)
+    (cond
+      ((null? lat) #f)
+      (else (is-first-b? (car lat) (cdr lat))))))
+
+;; First attempt at is-first?
+
+(define is-first?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (eq? a (car lat))))))
+
+;; Decision making in is-first? instead of two-in-a-row?
+
+(define is-first-b?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? a (car lat)) (old-two-in-a-row? lat))))))
+
+;; 
+
+(define two-in-a-row?
+  (lambda 
