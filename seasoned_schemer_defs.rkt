d677cf5fada6b44027ac697495b77a29d05bea66
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-04-11 13:59:02 -0700

    Finish two-in-a-row?

--- seasoned_schemer_defs.rkt
+++ seasoned_schemer_defs.rkt
@@ -21,3 +21,45 @@
     (cond
       ((null? lat) #f)
       (else (or (eq? a (car lat)) (member? a (cdr lat)))))))
+
+;; Let's find two in a row
+
+(define two-in-a-row?
+  (lambda (lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (is-first? (car lat) (cdr lat)) (two-in-a-row? (cdr lat)))))))
+
+(define is-first?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (eq? (car lat) a)))))
+
+;; Putting more responsibility into is-first?
+
+(define two-in-a-row2?
+  (lambda (lat)
+    (cond
+      ((null? lat) #f)
+      (else (is-first-b? (car lat) (cdr lat))))))
+
+(define is-first-b?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? a (car lat)) (two-in-a-row2? lat))))))
+
+;; Last run through
+
+(define two-in-a-row-b?
+  (lambda (preceeding lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? preceeding (car lat)) (two-in-a-row-b? (car lat) (cdr lat)))))))
+
+(define two-in-a-row-final?
+  (lambda (lat)
+    (cond
+      ((null? lat) #f)
+      (else (two-in-a-row-b? (car lat) (cdr lat))))))
